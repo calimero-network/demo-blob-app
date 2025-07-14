d2e135d5fbb08a96e52a1a491efe7732ca264903
alenmestrov <alenmestrov@gmail.com>
2025-07-14 10:59:23 +0200

    fix: Cargo.toml and build script

--- logic/build.sh
+++ logic/build.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname $0)"
 
-TARGET="${CARGO_TARGET_DIR:-../../target}"
+TARGET="${CARGO_TARGET_DIR:-target}"
 
 rustup target add wasm32-unknown-unknown
 
@@ -11,8 +11,8 @@ cargo build --target wasm32-unknown-unknown --profile app-release
 
 mkdir -p res
 
-cp $TARGET/wasm32-unknown-unknown/app-release/chat_app.wasm ./res/
+cp $TARGET/wasm32-unknown-unknown/app-release/demo_blob_app.wasm ./res/
 
 if command -v wasm-opt > /dev/null; then
-  wasm-opt -Oz ./res/chat_app.wasm -o ./res/chat_app.wasm
-fi 
+  wasm-opt -Oz ./res/demo_blob_app.wasm -o ./res/demo_blob_app.wasm
+fi
